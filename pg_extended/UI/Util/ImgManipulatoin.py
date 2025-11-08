d141b81ab90debbec3a7368b0a95675e5617bb4e
Saurabh262004 <saurabhrjadhav26@gmail.com>
2025-11-08 13:58:29 +0530

    move getGradient function to ImgManipulation class

--- pg_extended/UI/Util/ImgManipulatoin.py
+++ pg_extended/UI/Util/ImgManipulatoin.py
@@ -71,3 +71,28 @@ class ImgManipulation:
     result.blit(mask, (0, 0), special_flags=pg.BLEND_RGBA_MULT)
 
     return result
+
+  # make a 2xn or nx2 pixel surface with provided colors that can later be smoothscaled to get a color gradient
+  @staticmethod
+  def getGradient(colors: list[pg.Color | tuple[int, int, int] | tuple[int, int, int, int]], sizes: list[int], direction: str, thickness: int = 2) -> pg.Surface:
+    if direction in ('up', 'left'):
+      colors.reverse()
+      sizes.reverse()
+
+    if direction in ('up', 'down'):
+      w = thickness
+      h = sum(sizes)
+    else:
+      w = sum(sizes)
+      h = thickness
+
+    surface = pg.Surface((w, h), pg.SRCALPHA)
+
+    if direction in ('up', 'down'):
+      for i in range(len(colors)):
+        pg.draw.rect(surface, colors[i], (0, sum(sizes[:i]), thickness, sizes[i]), 0)
+    else:
+      for i in range(len(colors)):
+        pg.draw.rect(surface, colors[i], (sum(sizes[:i]), 0, sizes[i], thickness), 0)
+
+    return surface
