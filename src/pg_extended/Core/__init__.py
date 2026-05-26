f089a664e756755f3672eef2e78b5001a1176c31
Saurabh262004 <saurabhrjadhav26@gmail.com>
2026-05-26 17:47:35 +0530

    plans to move all rendering to a separate module

--- src/pg_extended/Core/__init__.py
+++ src/pg_extended/Core/__init__.py
@@ -1,5 +1,7 @@
 from pg_extended.Core import Composites
 from pg_extended.Core import Base
+from pg_extended.Core import Renderer
 
 from pg_extended.Core.Composites import *
 from pg_extended.Core.Base import *
+from pg_extended.Core.Renderer import *
