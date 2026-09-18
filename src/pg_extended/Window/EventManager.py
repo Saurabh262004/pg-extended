86cb53f62ddbd91e56a64d41f0958e4fa0c4af87
Saurabh262004 <saurabhrjadhav26@gmail.com>
2026-09-18 13:40:44 +0530

    erm

--- src/pg_extended/Window/EventManager.py
+++ src/pg_extended/Window/EventManager.py
@@ -34,6 +34,6 @@ class EventManager:
 						else:
 							self.activeSystems[systemID].handleEvents(event)
 
-				if not (cursorChange == self.activeCursor):
+				if cursorChange != self.activeCursor:
 					self.setCursor(CURSOR_CONSTANTS[cursorChange])
 					self.activeCursor = cursorChange
